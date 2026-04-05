b930d6ab9445bda0961c27faa228d7ac39b2a1ef
郝翠青 <haocuiqing@haocuiqingdeMacBook-Air.local>
2026-04-05 17:26:21 +0800

    feat: 初版单例

--- Singleton.java
+++ Singleton.java
@@ -1,3 +1,26 @@
 public class Singleton {
-    
+    private static final Singleton singleton = new Singleton();
+
+    private Singleton () {}
+
+    public Singleton getInstance () {
+        return singleton;
+    }
+}
+
+class Singleton2 {
+    private static volatile Singleton2 singleton;
+
+    private Singleton2 () {}
+
+    public Singleton2 getInstance () {
+        if (singleton == null) {
+            synchronized (Singleton2.class) {
+                if (singleton == null) {
+                    singleton = new Singleton2();
+                }
+            }
+        }
+        return singleton;
+    }
 }
