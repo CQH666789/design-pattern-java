33ed326e03715a41b319c0fea417316206af90ae
郝翠青 <haocuiqing@haocuiqingdeMacBook-Air.local>
2026-04-05 20:04:25 +0800

    feat: 生产者消费者

---
+++ Product.java
@@ -0,0 +1,43 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Product {
+    public static void main (String[] args) {
+        Deque<Integer> list = new LinkedList<>();
+        int CAPACITY = 5;
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                synchronized (list) {
+                    if (list.size() == CAPACITY) {
+                        try {
+                            list.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    list.addLast(i);
+                    System.out.println("生产了:" + i);
+                    list.notifyAll();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                synchronized(list) {
+                    if (list.isEmpty()) {
+                        try {
+                            list.wait();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    int cur = list.removeFirst();
+                    list.notifyAll();
+                    System.out.println("消费了:" + cur);
+                }
+            }
+        }).start();
+    }
+}
